96ab3a698ba18d7db2a14d36bf35a7b1d992fb03
LuminousAme <ame.gilham@ontariotechu.net>
2023-02-21 11:17:42 -0500

    fixed compile error from twice imported dll

---
+++ Assets/scripts/test/AirtableIntegrationTest.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirtableIntegrationTest : MonoBehaviour
+{
+    [Header("Airtable information")]
+    public string tableName;
+    public string recordId;
+}
